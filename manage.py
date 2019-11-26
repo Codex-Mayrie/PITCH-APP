11404697c4eb5b31ce3a505291330ee1d4f6d9e6
shee1998 <marywanjikumburu1@gmail.com>
2019-11-26 09:30:09 +0300

    create routes for registration and login form and render them appropriate, create login and register.html

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'WANJIKU254'
@@ -24,6 +25,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+     form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+     form = LoginForm()
+    return render_template('login.html', title='login', form=form)
   
   
   
