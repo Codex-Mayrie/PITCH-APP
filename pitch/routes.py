be3321e7b9f5120cdbe2d47b50ea59cd2b6c8483
shee1998 <marywanjikumburu1@gmail.com>
2019-11-27 09:25:17 +0300

    initialize the project

--- pitch/routes.py
+++ pitch/routes.py
@@ -1,13 +1,7 @@
-from datetime import datetime
-from flask import Flask, render_template, url_for, flash,redirect
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'WANJIKUMBURU'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
+from pitch.models import User, Pitch
+from flask import render_template, url_for, flash,redirect
+from pitch.forms import RegistrationForm, LoginForm
+from pitch import app
 
 pitches = [
     {
@@ -53,7 +47,3 @@ def login():
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
