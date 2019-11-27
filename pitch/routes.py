46a20b2a661cf44612260a81767876678f651d37
shee1998 <marywanjikumburu1@gmail.com>
2019-11-27 10:18:04 +0300

    create validation for logging in

--- pitch/routes.py
+++ pitch/routes.py
@@ -1,7 +1,7 @@
 from pitch.models import User, Pitch
 from flask import render_template, url_for, flash,redirect
 from pitch.forms import RegistrationForm, LoginForm
-from pitch import app
+from pitch import app, db, bcrypt
 
 pitches = [
     {
@@ -32,8 +32,12 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in','success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
