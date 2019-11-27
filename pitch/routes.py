592a11aaae89b4547d7e20c9bdaad804fd7bf749
shee1998 <marywanjikumburu1@gmail.com>
2019-11-27 15:28:23 +0300

    recreate the folder structure

--- pitch/routes.py
+++ pitch/routes.py
@@ -1,17 +1,21 @@
-from pitch.models import User, Pitch
-from flask import render_template, url_for, flash,redirect
-from pitch.forms import RegistrationForm, LoginForm
+from flask import render_template, url_for, flash, redirect, request
 from pitch import app, db, bcrypt
+from pitch.forms import RegistrationForm, LoginForm
+from pitch.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
-pitches = [
+
+posts = [
     {
         'author': 'Corey Schafer',
-        'title': 'First Pitch',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
     {
         'author': 'Jane Doe',
-        'title': 'Second Pitch',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
         'date_posted': 'April 21, 2018'
     }
 ]
@@ -20,7 +24,7 @@ pitches = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', pitches=pitches)
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
@@ -30,24 +34,42 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash('Your account has been created! You are now able to log in','success')
+        flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
